ac8721acd235e8afa2141f95a6555a6251f0b9ff
Mawais200 <fa22-bsse-201@lgu.edu.pk>
2025-02-23 04:07:31 +0500

    use anchor tag

--- React02/src/assets/Card.jsx
+++ React02/src/assets/Card.jsx
@@ -22,7 +22,7 @@ function Card({title, price}) {
             <div className="text-center text-yellow-500 border-black border-2 max-w-[full] mx-auto mb-[40px] w-[300px] ">
                 <h1 className="text-[36px] font-semibold">{title} </h1> 
                 <p className="text-[18px] ">Price is {price}</p>
-                {price>3000? <p>You will get price discount</p> : null}
+                {price>3000? <p>You will get price discount</p> : <a href="">Get Discount</a>}
                
             </div>
         </div>
