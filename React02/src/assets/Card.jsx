d679cff61db02271097e6029e759d0d1d9cd0684
Mawais200 <fa22-bsse-201@lgu.edu.pk>
2025-02-23 03:42:48 +0500

    added array as a prop

--- React02/src/assets/Card.jsx
+++ React02/src/assets/Card.jsx
@@ -1,18 +1,17 @@
 
 
 
+function Card({title, decription}) {
+    let option = ["AWAIS", "AWAIS", "AWAIS"];
 
 
-function Card({title, decription,feature}) {
-
-    return(
+    return (
         <div className="text-center ">
-            <div className="text-center  text-yellow-500 border-black border-2 max-w-[full] mx-auto mb-[40px] w-[300px] ">
-              <h1 className="text-[36px] font-semibold">{title} </h1> 
-              <p className="text-[18px] ">{decription}</p>
-              <p className="text-[18px] ">{feature}</p>  
-              
+            <div className="text-center text-yellow-500 border-black border-2 max-w-[full] mx-auto mb-[40px] w-[300px] ">
+                <h1 className="text-[36px] font-semibold">{title} </h1> 
+                <p className="text-[18px] ">{decription}</p>
+                <ul className="list-none text-[18px]">Features is :{option.map((item, index) => <li key={index}>{item}</li>)}</ul>  
             </div>
-            </div>
+        </div>
     )}
 export default Card;
--- React02/src/assets/CardTab.jsx
+++ React02/src/assets/CardTab.jsx
@@ -2,9 +2,11 @@ import Card from './Card'
 
 function CardTab(){
     let option = ["awais", "Ali", "Naveed", "abdullah"]
+    // let option =[<li>"AWAIS"</li>,<li>"AWAIS"</li>,<li>"AWAIS"</li>,]
+
     return(
         <div>
-            <Card title="Game Developer" decription={2} feature={option}/>
+            <Card title="Game Developer" decription={2} features={option}/>
             <Card title="App Developer" decription="Learn React Native,Flutter,Angular"/>
         </div>
     )
